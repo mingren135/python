8fd33df2ce0175da346970ad7ee2d79354f7acad
haixiang <haixiang@microants.cn>
2018-06-20 17:39:56 +0800

    add suning

--- ms/sn_ms.py
+++ ms/sn_ms.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
- 
+
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
+driver.get("https://passport.suning.com/ids/login")
+
+elemtab = driver.find_element_by_xpath("/html/body/div[2]/div/div/div[1]/a[2]/span")
+elemtab.click()
+elem = driver.find_element_by_id("userName")
+elem.send_keys("17316901545")
+
+elem2 = driver.find_element_by_id("password")
+elem2.send_keys("asus135")
 elem.send_keys(Keys.RETURN)
 print(driver.page_source)
